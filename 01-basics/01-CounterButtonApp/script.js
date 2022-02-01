1d37c8935c4b7e55b73655476a35545551940e34
Yuri Kuichik <kuichikmis4enko@gmail.com>
2022-02-01 18:23:38 +0300

    увеличение на 1 по клику на кнопку

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,17 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+ // import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const Counter = {
+   data() {
+     return {
+       count: 0,
+     }
+   },
+   methods: {
+     plusOneCount () {
+       this.count ++
+     }
+   }
+   }
+
+   Vue.createApp(Counter).mount('#app')
