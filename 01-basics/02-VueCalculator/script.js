2a84fa9d335099d8fd8709044fb8c41d8cda9811
Yuri Kuichik <kuichikmis4enko@gmail.com>
2022-02-15 18:11:01 +0300

    Vue Calculator (правка)

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -6,42 +6,29 @@ import { defineComponent, createApp } from './vendor/vue.esm-browser.js';
 
     data() {
       return {
-        elementsCalculator: {
-          firstNum: 0,
-          secondNum: 0,
-          operations: {
-            sum: true,
-            subtract: false,
-            multiply: false,
-            divide: false,
-          },
-        },
+        operator: '',
+        firstNum: 0,
+        secondNum: 0,
       }
     },
     computed: {
       result(){
-        return (this.handleResult() > 0 || this.handleResult() < 0)  && this.handleResult() !== Infinity ? this.handleResult() : 0
+        return !Number.isNaN(this.handleResult()) && this.handleResult() !== Infinity ? this.handleResult() : 0   // исключение в result Infinity и NaN ( при 0 / 0 )
       }
     },
     methods: {
       handleResult() {
-         let firstNum = this.elementsCalculator.firstNum;
-         let secondNum = this.elementsCalculator.secondNum;
-         if (this.elementsCalculator.operations.sum) {
-           return firstNum + secondNum
-         } else if (this.elementsCalculator.operations.subtract) {
-           return firstNum - secondNum
-         } else if (this.elementsCalculator.operations.multiply) {
-           return firstNum * secondNum
-         } else if (this.elementsCalculator.operations.divide) {
-           return firstNum / secondNum
-         }
-      },
-      changeOperation(opName) {
-        this.elementsCalculator.operations.sum = opName === 'sum'
-        this.elementsCalculator.operations.subtract = opName === 'subtract'
-        this.elementsCalculator.operations.multiply = opName === 'multiply'
-        this.elementsCalculator.operations.divide = opName === 'divide'
+         let firstNum = this.firstNum;
+         let secondNum = this.secondNum;
+        if (this.operator === "sum") {
+          return firstNum + secondNum
+        } else if (this.operator === "subtract") {
+          return firstNum - secondNum
+        } else if (this.operator === "multiply") {
+          return firstNum * secondNum
+        } else if (this.operator === "divide") {
+          return firstNum / secondNum
+        }
       }
     }
   })
