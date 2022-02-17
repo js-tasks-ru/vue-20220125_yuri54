3847956a52e3d115e3d8ae942057840e1fb5fdf7
Yuri Kuichik <kuichikmis4enko@gmail.com>
2022-02-17 23:49:14 +0300

    MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,42 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+ import {defineComponent, createApp } from './vendor/vue.esm-browser.js';
 
-const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
+
+const MeetupTitle = defineComponent( {
+  name: 'MeetupTitle',
+  data() {
+    return {
+      meetupId: '',
+      meetupTitle: ''
+    }
+  },
+  watch: {
+    meetupId(newValue, oldValue) {
+      this.fetchMeetupById(newValue)
+    }
+ },
+  methods: {
+    fetchMeetupById(meetupId) {
+      const API_URL = 'https://course-vue.javascript.ru/api';
+      return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+        if (response.ok) {
+          return response.json().then((result) => {
+            this.meetupTitle = result.title;
+          });
+        } else {
+          return response.json().then((error) => {
+            throw error;
+          });
+        }
       });
     }
-  });
-}
+  }
+
+})
+
+const app = createApp(MeetupTitle)
+
+const vm = app.mount('#app')
+ window.vm = vm;
 
 // Требуется создать Vue приложение
