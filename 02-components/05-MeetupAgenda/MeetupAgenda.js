11a789d974f2d97b97391acb0b3f9830d3ada9c4
Yuri Kuichik <kuichikmis4enko@gmail.com>
2022-03-04 23:54:49 +0300

    MeetupAgenda

--- 02-components/05-MeetupAgenda/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/MeetupAgenda.js
@@ -1,13 +1,32 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
 
+  components: {
+    MeetupAgendaItem
+  },
+
+  props: {
+    agenda: {
+      type: Array,
+      required: true
+    }
+  },
+
   template: `
     <ul class="agenda">
       <li class="agenda__item">
-        <!-- meetup agenda item -->
+        <div>
+          <meetup-agenda-item :agenda-item="agenda[0]"></meetup-agenda-item>
+        </div>
+        <div>
+          <meetup-agenda-item :agenda-item="agenda[1]"></meetup-agenda-item>
+        </div>
+        <div>
+          <meetup-agenda-item :agenda-item="agenda[2]"></meetup-agenda-item>
+        </div>
       </li>
     </ul>`,
 });
